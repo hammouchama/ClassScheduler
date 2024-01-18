6a724968aef881f426fd04e50b983caf8b1ba25c
MarwanEA <marwanelaoumrani@gmail.com>
2024-01-18 22:00:35 +0100

    Added homepage, login page, header, footer

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './user/home/home-main/home.component';
+// import { LoginComponent } from './login/login.component';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
 import { AssistantComponent } from './assistant/assistant.component';
@@ -9,16 +9,32 @@ import { AuthGuard } from './auth/auth.guard';
 import { ListAssistanComponent } from './admin/list-assistan/list-assistan.component';
 import { ForbiddenComponent } from './forbidden/forbidden.component';
 import { AssistantInfoComponent } from './admin/assistant-info/assistant-info.component';
+import { SignInMainComponent } from './user/sign-in/sign-in-main/sign-in-main.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: '', component: HomeComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'admin', component: AdminComponent, canActivate: [AuthGuard], data: { role: 'Admin' } },
-  { path: 'admin/assistant', component: ListAssistanComponent, canActivate: [AuthGuard], data: { role: 'Admin' } },
-  { path: 'admin/assistant/info/:id', component: AssistantInfoComponent, canActivate: [AuthGuard], data: { role: 'Admin' } },
+  // { path: 'login', component: LoginComponent },
+  { path: 'login2', component: SignInMainComponent },
+  {
+    path: 'admin',
+    component: AdminComponent,
+    canActivate: [AuthGuard],
+    data: { role: 'Admin' },
+  },
+  {
+    path: 'admin/assistant',
+    component: ListAssistanComponent,
+    canActivate: [AuthGuard],
+    data: { role: 'Admin' },
+  },
+  {
+    path: 'admin/assistant/info/:id',
+    component: AssistantInfoComponent,
+    canActivate: [AuthGuard],
+    data: { role: 'Admin' },
+  },
   { path: 'forbidden', component: ForbiddenComponent },
-
 ];
 
 @NgModule({

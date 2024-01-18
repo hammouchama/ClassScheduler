7dc74ba3d5377f420187e9736bdad42d3717fe59
MarwanEA <marwanelaoumrani@gmail.com>
2024-01-18 22:04:00 +0100

    Added homepage, login page, header, footer

---
+++ frontend/src/app/user/sign-in/sign-in-area/sign-in-area.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { UserService } from '../../../service/user.service';
+import { UserAuthService } from '../../../service/user-auth.service';
+import { Router } from '@angular/router';
+@Component({
+  selector: 'app-sign-in-area',
+  templateUrl: './sign-in-area.component.html',
+  styleUrls: ['./sign-in-area.component.scss'],
+})
+export class SignInAreaComponent implements OnInit {
+  hide: any;
+  email: any;
+  errorMessa: string = 'ddd';
+  error: boolean = false;
+
+  constructor(
+    private userService: UserService,
+    private userAuthService: UserAuthService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {}
+  login(loginForm: NgForm) {
+    //! console.log(loginForm.value)
+    this.userService.login(loginForm.value).subscribe(
+      (response: any) => {
+        //console.log(response)
+        //? save the token and roles in localStorage
+        this.userAuthService.setToken(response.token);
+        const role = response.role;
+        this.userAuthService.setRole(role);
+        //! redarect
+        role === 'Admin'
+          ? this.router.navigate(['/admin'])
+          : role === 'assistant'
+          ? this.router.navigate(['/assistant'])
+          : this.router.navigate(['/user']);
+      },
+      (error) => {
+        this.error = true;
+        this.errorMessa = error.error.message;
+        console.log(error.error.message);
+      }
+    );
+  }
+}

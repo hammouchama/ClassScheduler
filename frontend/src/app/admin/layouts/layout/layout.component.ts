99ba80e223a2c3cc836c70991df09a2a146a21e4
MarwanEA <marwanelaoumrani@gmail.com>
2024-01-20 21:37:17 +0100

    Added homepage, login page, header, footer

---
+++ frontend/src/app/admin/layouts/layout/layout.component.ts
@@ -0,0 +1,41 @@
+import { Component, Inject, OnInit } from '@angular/core';
+
+
+import { LAYOUT_VERTICAL, LAYOUT_HORIZONTAL } from './layouts.model';
+import { EventService } from 'src/app/service/event.service';
+
+@Component({
+  selector: 'app-layout',
+  templateUrl: './layout.component.html',
+  styleUrls: ['./layout.component.scss'],
+})
+export class LayoutComponent implements OnInit {
+  // layout related config
+  layoutType!: string;
+
+  constructor(private eventService: EventService) {}
+
+  ngOnInit() {
+    // default settings
+    document.body.setAttribute('data-bs-theme', 'light');
+    this.layoutType = LAYOUT_VERTICAL;
+    // listen to event and change the layout, theme, etc
+    this.eventService.subscribe('changeLayout', (layout: string) => {
+      this.layoutType = layout;
+    });
+  }
+
+  /**
+   * Check if the vertical layout is requested
+   */
+  isVerticalLayoutRequested() {
+    return this.layoutType === LAYOUT_VERTICAL;
+  }
+
+  /**
+   * Check if the horizontal layout is requested
+   */
+  isHorizontalLayoutRequested() {
+    return this.layoutType === LAYOUT_HORIZONTAL;
+  }
+}
